f4e14b62edf1246d5e8364429b2edf23b6d0dca7
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-01-20 18:07:06 -0500

    Added function to delete note

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -11,17 +11,36 @@ const NoteState = (props) => {
 
 
   // add notes------------------------------------------------
-  const addNote = (title, description, tag) => {
-    let newNote = {
-      "_id": "65934c14cb22da32d1eb7eaj",
-      "user": "659337031a9e1ef5d61b0cd6",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2024-01-01T23:34:44.515Z",
-      "__v": 0
-    }
-    setNotes(notes.concat(newNote))  //array.concat returns a new array so this will set value of state to new array
+  const addNote = async(title, description, tag) => {
+    //api call
+    let url = host + "/user/notes/addNote"
+
+    //api call
+    const response = await fetch(url, {
+      method: "POST", // 
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5MzM3MDMxYTllMWVmNWQ2MWIwY2Q2In0sImlhdCI6MTcwNDE0NjY5MX0.xXkl0EetMBXmDsWJMb-gNLU9RMiV5pZ_QSnd5ih9xiU"
+      },
+      body: JSON.stringify({ title, description, tag }),
+    });
+    let json = await response.json()
+    console.log(json)
+
+
+
+
+
+    // let newNote = {
+    //   "_id": "65934c14cb22da32d1eb7eaj",
+    //   "user": "659337031a9e1ef5d61b0cd6",
+    //   "title": title,
+    //   "description": description,
+    //   "tag": tag,
+    //   "date": "2024-01-01T23:34:44.515Z",
+    //   "__v": 0
+    // }
+    // setNotes(notes.concat(newNote))  //array.concat returns a new array so this will set value of state to new array
 
   }
 
@@ -48,12 +67,28 @@ const NoteState = (props) => {
 
 
   //delete Note-----------------------------------------------
-  const deleteNote = (id) => {
+  const deleteNote = async(id) => {
     // console.log("Deleting note with id :" +id)
-    let newNotes = notes.filter((note) => {
-      return note._id !== id
-    })
-    setNotes(newNotes)
+
+    //api call
+
+    let url = host + "/user/notes/deleteNote/" + id
+
+    //api call
+    const response = await fetch(url, {
+      method: "DELETE", // 
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5MzM3MDMxYTllMWVmNWQ2MWIwY2Q2In0sImlhdCI6MTcwNDE0NjY5MX0.xXkl0EetMBXmDsWJMb-gNLU9RMiV5pZ_QSnd5ih9xiU"
+      },
+    });
+
+
+    getNotes()
+    // console.log("note deleted")
+
+
+  
   }
 
   //update Note-----------------------------------------------
